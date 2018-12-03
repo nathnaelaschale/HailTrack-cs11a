25ed85d22cf64f7bf2a461efbd08f582ec69ecaf
AiriWang <44778727+AiriWang@users.noreply.github.com>
2018-12-02 21:19:04 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,22 @@
+/**
+ * 
+ * 
+ * 
+ */
+
 import java.util.*;
 
 public class TicTacToe {
 	public static Scanner console = new Scanner(System.in);
 	public static String firstPlayer = "";
 	public static String secondPlayer = "";
-//Basic prompt for which game the players want to play.
+
 	public static void main(String[] args) {
 		System.out.println("Hello! You are currently playing HailTrack's TicTacToe. Which game "
 				+ "would you like to play? Input to number corresponding to your choice.");
 		System.out.println("1  Normal TicTacToe");
 		System.out.println("2  3D TicTacToe");
 		int choice = console.nextInt();
-		//While loop that prompts the user if they don't enter 1 or 2.
 		while (choice != 1 && choice != 2) {
 			System.out.println("Error: Please enter the number corresponding to the game of your choice.");
 			choice = console.nextInt();
@@ -24,32 +29,29 @@ public class TicTacToe {
 		}
 	}
 
+	/* */
+	
 	public static void NormalTicTacToe() {
-
-	}
-	//prints the board and explains the rules of the game.
-	public static void ThreeDTicTacToe() {
-		String[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
-				"18", "19", "20", "21", "22", "23", "24", "25", "26", "27" };
 		boolean PlayAgain = true;
 		System.out.println("Normal Tic-Tac-Toe");
 		System.out.println("________________________________");
-		System.out.println("This is a 2-player game with a 2-dimentional 3 by 3 Tic-Tac-Toe board.");
+		System.out.println("This is a 2-player game with a 2-dimensional 3 by 3 Tic-Tac-Toe board.");
 		System.out.println("First person to get 3 in a row wins!");
 		System.out.println("_____________________");
 		System.out.println("How to play: the computer will display the board, and there will "
 				+ "be a number corresponding to each empty space. When prompted, enter the "
 				+ "number corresponding to the space where you want to place your piece. Your "
 				+ "piece will show up on the board! Have fun :)");
-		//Prompts the players to enter in their player names and turn order.
 		while (PlayAgain == true) {
-			System.out.println("Enter name of player one:");
-			String p1 = console.nextLine();
-			System.out.println("Enter name of player two:");
-			String p2 = console.nextLine();
+			String[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+			int counter = 1;
+			System.out.println("Enter name of player one: ");
+			String p1 = console.next();
+			System.out.println("Enter name of player two: ");
+			String p2 = console.next();
 			System.out.println("Who wants to go first? ");
-			firstPlayer = console.nextLine();
-			if (firstPlayer == p1) {
+			firstPlayer = console.next();
+			if (firstPlayer.equals(p1)) {
 				secondPlayer = p2;
 			} else {
 				secondPlayer = p1;
@@ -57,65 +59,126 @@ public class TicTacToe {
 			printBoards(board);
 			boolean win = false;
 			do {
-				firstplay(board, firstPlayer);
-				if (winThreeByThreeTest(board, win) == true || winCombinedBoardTest(board, win) == true) {
+				firstplay(board);
+				win = winThreeByThreeTest(board);
+				if (win == true) {
 					break;
 				}
-				secondplay(board, secondPlayer);
-				if (winThreeByThreeTest(board, win) == true || winCombinedBoardTest(board, win) == true) {
+				if (counter > 8) {
 					break;
 				}
+				counter++;
+				secondplay(board);
+				win = winThreeByThreeTest(board);
+				if (win == true) {
+					break;
+				}
+				counter++;
 			} while (win == false);
-		}
-		//Checks to see if the players want to play again. If yes it creates a new game and if no it ends the program.
-		System.out.println("Would you like to play again? (Answer 'yes' or 'no')");
-		String YesNo = console.nextLine();
-		while (!YesNo.equalsIgnoreCase("yes") && !YesNo.equalsIgnoreCase("no"))
+			if (win == false){
+				System.out.println("Nice work, but it's a tie! Great game!");
+			}
+			System.out.println("Would you like to play again? (Answer 'yes' or 'no')");
+			String YesNo = console.next();
+			while (!YesNo.equalsIgnoreCase("yes") && !YesNo.equalsIgnoreCase("no")) {
+				System.out.println("Error: Please enter 'yes' or 'no'.");
+				YesNo = console.next();
+			}
 			if (YesNo.equalsIgnoreCase("no")) {
 				PlayAgain = false;
 			}
+		}
 	}
-	/*For the player going first (The X) it does a few important things: prompts the player for their move.
-	Then checks to see if the move has already been made or does not exist and informs the user.
-	If the move is acceptable it goes through the printboard method. */
-	public static void firstplay(String[] board, String currentPlayer) {
-		System.out.println(currentPlayer + " make your move:");
+
+	public static void ThreeDTicTacToe() {
+		boolean PlayAgain = true;
+		System.out.println("3D Tic-Tac-Toe");
+		System.out.println("________________________________");
+		System.out.println("This is a 2-player game with a 3-dimensional 3 by 3 by 3 Tic-Tac-Toe board.");
+		System.out.println("First person to get 3 in a row wins!");
+		System.out.println("_____________________");
+		System.out.println("How to play: the computer will display the board, and there will "
+				+ "be a number corresponding to each empty space. When prompted, enter the "
+				+ "number corresponding to the space where you want to place your piece. Your "
+				+ "piece will show up on the board! Have fun :)");
+		while (PlayAgain == true) {
+			String[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+					"18", "19", "20", "21", "22", "23", "24", "25", "26", "27" };
+			System.out.println("Enter name of player one: ");
+			String p1 = console.next();
+			System.out.println("Enter name of player two: ");
+			String p2 = console.next();
+			System.out.println("Who wants to go first? ");
+			firstPlayer = console.next();
+			if (firstPlayer.equals(p1)) {
+				secondPlayer = p2;
+			} else {
+				secondPlayer = p1;
+			}
+			printBoards(board);
+			boolean win = false;
+			do {
+				firstplay(board);
+				if (winThreeByThreeTest(board) == true || winCombinedBoardTest(board) == true) {
+					win = true;
+				}
+				if (win == true) {
+					break;
+				}
+				secondplay(board);
+				if (winThreeByThreeTest(board) == true || winCombinedBoardTest(board) == true) {
+					win = true;
+				}
+				if (win == true) {
+					break;
+				}
+			} while (win == false);
+			System.out.println("Would you like to play again? (Answer 'yes' or 'no')");
+			String YesNo = console.next();
+			while (!YesNo.equalsIgnoreCase("yes") && !YesNo.equalsIgnoreCase("no")) {
+				if (YesNo.equalsIgnoreCase("no")) {
+					PlayAgain = false;
+				}
+			}
+		}
+	}
+
+	public static void firstplay(String[] board) {
+		System.out.println(firstPlayer + " make your move:");
 		Scanner number = new Scanner(System.in);
 		int move = number.nextInt();
 		move--;
 		if (move < 0 || move > board.length) {
 			System.out.println("That spot does not exist, make another move");
-			firstplay(board, currentPlayer);
+			firstplay(board);
 		}
 		if (board[move] != "O" && board[move] != "X") {
 			board[move] = "X";
 		} else {
 			System.out.println("That spot is already taken, make another move.");
-			firstplay(board, currentPlayer);
+			firstplay(board);
 		}
 		printBoards(board);
 	}
-	/*For the player going second(The O) does a few important things: prompts the player for their move.
-Then checks to see if the move has already been made or does not exist and informs the user.
-If the move is acceptable it goes through the printboard method.*/
-	public static void secondplay(String[] board, String currentPlayer) {
-		System.out.println(currentPlayer + " make your move:");
+
+	public static void secondplay(String[] board) {
+		System.out.println(secondPlayer + " make your move:");
 		Scanner number = new Scanner(System.in);
 		int move = number.nextInt();
 		move--;
 		if (move < 0 || move > board.length) {
 			System.out.println("That spot does not exist, make another move");
-			firstplay(board, currentPlayer);
+			secondplay(board);
 		}
 		if (board[move] != "O" && board[move] != "X") {
 			board[move] = "O";
 		} else {
 			System.out.println("That spot is already taken, make another move.");
-			secondplay(board, currentPlayer);
+			secondplay(board);
 		}
 		printBoards(board);
 	}
-	//Updates the board with player moves at the beginning of each player's turn.
+
 	public static void printBoards(String[] board) {
 		for (int i = 0; i < board.length; i += 3) {
 			System.out.println("-------------");
@@ -126,8 +189,9 @@ If the move is acceptable it goes through the printboard method.*/
 			}
 		}
 	}
-	//The method that tests to see if a player won.
-	public static boolean winThreeByThreeTest(String[] board, Boolean win) {
+
+	public static boolean winThreeByThreeTest(String[] board) {
+		boolean win = false;
 		for (int i = 0; i < board.length; i += 3) {
 			if (board[i].equals("X") && board[i + 1].equals("X") && board[i + 2].equals("X")) {
 				win = true;
@@ -149,10 +213,27 @@ If the move is acceptable it goes through the printboard method.*/
 				i += 6;
 			}
 		}
+		if (board[0].equals("X") && board[4].equals("X") && board[8].equals("X")) {
+			win = true;
+			System.out.println("Congratulations," + firstPlayer + " won!!!!!");
+		}
+		if (board[2].equals("X") && board[4].equals("X") && board[6].equals("X")) {
+			win = true;
+			System.out.println("Congratulations," + firstPlayer + " won!!!!!");
+		}
+		if (board[0].equals("O") && board[4].equals("O") && board[8].equals("O")) {
+			win = true;
+			System.out.println("Congratulations," + secondPlayer + " won!!!!!");
+		}
+		if (board[2].equals("O") && board[4].equals("O") && board[6].equals("O")) {
+			win = true;
+			System.out.println("Congratulations," + secondPlayer + " won!!!!!");
+		}
 		return win;
 	}
 
-	public static boolean winCombinedBoardTest(String[] board, Boolean win) {
+	public static boolean winCombinedBoardTest(String[] board) {
+		boolean win = false;
 		for (int i = 0; i < 9; i++) {
 			if (board[i].equals("X") && board[i + 9].equals("X") && board[i + 18].equals("X")) {
 				win = true;
